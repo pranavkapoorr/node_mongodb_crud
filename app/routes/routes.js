4a155e5cea207806e04111e331fd21bd4fdb9a71
Pranav Kapoor <pranavkapoorr@gmail.com>
2018-08-28 22:28:05 +0100

    push code

---
+++ app/routes/routes.js
@@ -0,0 +1,52 @@
+var ObjectID = require('mongodb').ObjectID;
+
+module.exports = function(app, db) {
+    //create
+    app.post('/notes', (req, res) => {
+        const note = { text: req.body.body, title: req.body.title };
+        db.collection('notes').insert(note, (err, result) => {
+            if (err) { 
+                res.send({ 'error': 'An error has occurred' }); 
+            } else {
+                res.send(result.ops[0]);
+            }
+        });
+    });
+    //retreive
+    app.get('/notes/:id', (req, res) => {
+        const id = req.params.id;
+        const details = { '_id': new ObjectID(id) };
+        db.collection('notes').findOne(details, (err, item) => {
+            if (err) {
+                res.send({'error':'An error has occurred'});
+            } else {
+                    res.send(item!=null?item:"Item doesn't exist!");
+            }
+        });
+    });
+    //update
+    app.put('/notes/:id', (req, res) => {
+        const id = req.params.id;
+        const details = { '_id': new ObjectID(id) };
+        const note = { text: req.body.body, title: req.body.title };
+        db.collection('notes').update(details, note, (err, result) => {
+          if (err) {
+              res.send({'error':'An error has occurred'});
+          } else {
+              res.send(note);
+          } 
+        });
+      });
+      //delete
+      app.delete('/notes/:id', (req, res) => {
+        const id = req.params.id;
+        const details = { '_id': new ObjectID(id) };
+        db.collection('notes').remove(details, (err, item) => {
+          if (err) {
+            res.send({'error':'An error has occurred'});
+          } else {
+            res.send('Note ' + id + ' deleted!');
+          } 
+        });
+      });
+};
---
+++ config/db.js
@@ -0,0 +1,3 @@
+module.exports = {
+    url : "mongodb://admin:admin12345@ds137102.mlab.com:37102/node_mongo_crud"
+};
